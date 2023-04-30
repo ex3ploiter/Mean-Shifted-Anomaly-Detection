86d29cf7852f970886893b319186c39a21db1e1d
ex3ploiter <52802071+ex3ploiter@users.noreply.github.com>
2023-04-30 16:04:18 +0330

    path

--- utils.py
+++ utils.py
@@ -157,8 +157,8 @@ def get_loaders_normal(dataset, label_class, batch_size, backbone):
             path2='/mnt/new_drive/Sepehr/chest_xray/test'
 
         elif dataset == "Head-CT" :# 1
-            path1='../HEAD_CT/Train'
-            path2='../HEAD_CT/Test'
+            path1='/mnt/new_drive/Masoud_WorkDir/Head-CT/TrainWithAnomal/Train'
+            path2='/mnt/new_drive/Masoud_WorkDir/Head-CT/TrainWithAnomal/Test'
 
 
 
